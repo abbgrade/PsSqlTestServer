a47a2cc6c53bf5b77c3be17bbe732e3173c29c87
Steffen Kampmann <steffen.kampmann@gmail.com>
2021-12-16 12:16:46 +0100

    moved docs to inline

--- Source/Public/Remove-DockerSqlServer.ps1
+++ Source/Public/Remove-DockerSqlServer.ps1
@@ -1,10 +1,27 @@
 
 function Remove-DockerSqlServer {
 
-    [CmdletBinding()]
+    <#
+
+    .SYNOPSIS
+    Removes a test SQL server Docker container.
+
+    .DESCRIPTION
+    Removes a SQL server Docker container, in case it was created with `New-DockerSqlServer`.
+
+    .EXAMPLE
+    PS> $server = New-DockerSqlServer -DockerContainerName 'Sandbox' -ServerAdminPassword 'pa$$w0rd' -AcceptEula
+    PS> Remove-DockerSqlServer -DockerContainerName $server.Name
+
+    #>
+
+    [CmdletBinding( PositionalBinding=$false )]
     param (
+
+        # Specifies the name of the new Docker container.
         [Parameter( Mandatory )]
         [string] $DockerContainerName
+
     )
 
     Import-Module PsDocker -MinimumVersion 1.5.0 -ErrorAction Stop
--- Source/Public/Remove-SqlServer.ps1
+++ Source/Public/Remove-SqlServer.ps1
@@ -1,7 +1,22 @@
 function Remove-SqlServer {
 
-    [CmdletBinding()]
+    <#
+
+    .SYNOPSIS
+    Removes a test SQL server.
+
+    .DESCRIPTION
+    Removes a SQL server, in case it was created with `New-SqlServer`.
+
+    .EXAMPLE
+    PS> $server = New-SqlServer
+    PS> Remove-SqlServer -Server $server
+
+    #>
+
+    [CmdletBinding( PositionalBinding=$false )]
     param (
+        # Specifies the server to remove.
         $Server
     )
 
