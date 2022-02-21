b77c19af70981f6203206b9e62c3874a87a58925
Steffen Kampmann <steffen.kampmann@gmail.com>
2022-02-21 20:55:20 +0100

    updated build script

--- .Build.ps1
+++ .Build.ps1
@@ -3,15 +3,31 @@
 	Build script <https://github.com/nightroman/Invoke-Build>
 
 .Example
-	Invoke-Build Install
+    # Create module from source.
+    Invoke-Build Build
 
 .Example
-    Invoke-Build Publish -NuGetApiKey xyz
+    # Add doc templates for new command.
+    # BE CAREFUL! Existing documents will be overwritten and must be discarded using git.
+    Invoke-Build Doc.Init -ForceDocInit
 #>
 
 param(
-    [string] $NuGetApiKey # mandatory for Publish
+	[string] $NuGetApiKey = $env:nuget_apikey,
+
+	# Overwrite published versions
+	[switch] $ForcePublish,
+
+    # Add doc templates for new command.
+	[switch] $ForceDocInit,
+
+	# Version suffix to prereleases
+	[int] $BuildNumber
 )
 
-. $PsScriptRoot\Tasks\Build.Tasks.ps1
+$ModuleName = 'PsSqlTestServer'
 
+. $PSScriptRoot\tasks\Build.Tasks.ps1
+
+# Synopsis: Default task.
+task . Build
