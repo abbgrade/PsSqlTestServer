09b2c4537a5638849f0aac975c95d2a0499ca621
Steffen Kampmann <steffen.kampmann@gmail.com>
2022-03-15 22:21:55 +0100

    made PsSqlLocalDb optional

--- Test/Test-LocalDb.Tests.ps1
+++ Test/Test-LocalDb.Tests.ps1
@@ -2,12 +2,18 @@
 
 Describe 'Test-LocalDb' {
 
+    BeforeDiscovery {
+        $Script:PsSqlLocalDbModule = Import-Module PsSqlLocalDb -PassThru -ErrorAction SilentlyContinue
+    }
+
     BeforeAll {
         Import-Module $PSScriptRoot\..\Source\PsSqlTestServer.psd1 -Force -ErrorAction Stop
     }
 
-    It 'works' {
-        Test-LocalDb | Should -BeIn $true, $false
-    }
+    Context 'PsSqlLocalDb' -Skip:(-Not $Script:PsSqlLocalDbModule) {
 
+        It 'works' {
+            Test-LocalDb | Should -BeIn $true, $false
+        }
+    }
 }
