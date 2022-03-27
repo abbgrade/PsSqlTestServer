c707712dc94cc104e647bb758d008eb59cc1d3ab
Steffen Kampmann <steffen.kampmann@gmail.com>
2022-03-27 14:20:40 +0200

    added tests for New-SqlTestLocalInstance

--- test/New-LocalInstance.Tests.ps1
+++ test/New-LocalInstance.Tests.ps1
@@ -7,6 +7,26 @@ Describe 'New-LocalInstance' -Tag SqlLocalDB {
     }
 
     Context 'LocalDb' -Skip:( -Not ( Test-SqlTestLocalDb )) {
+
+        It 'Provides a SQL server' {
+            $Script:Instance = New-SqlTestLocalInstance
+
+            $Script:Instance | Should -Not -BeNullOrEmpty
+            $Script:Instance.ConnectionString | Should -Not -BeNullOrEmpty
+            $Script:Instance.DataSource | Should -Not -BeNullOrEmpty
+        }
+
+        It 'Provides unique SQL servers' {
+            $Script:Instance = @((New-SqlTestLocalInstance), (New-SqlTestLocalInstance))
+            $Script:Instance.Count | Should -Be 2
+            $Script:Instance[0].DataSource | Should -Not -Be $Script:Instance[1].DataSource -Because 'two instances must have unique names'
+        }
+
+        AfterEach {
+            if ( $Script:Instance ) {
+                $Script:Instance | Remove-SqlTestInstance
+            }
+        }
     }
 
 }
