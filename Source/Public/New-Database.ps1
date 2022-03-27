58867682c40c9c1b6f05d44ab50f8c9d5ab2d4a0
Steffen Kampmann <steffen.kampmann@gmail.com>
2022-03-27 09:56:04 +0200

    added new database command

---
+++ Source/Public/New-Database.ps1
@@ -0,0 +1,26 @@
+function New-Database {
+
+    [CmdletBinding()]
+    param (
+        [Parameter( Mandatory )]
+        [ValidateNotNullOrEmpty()]
+        [PsObject] $Instance,
+
+        [Parameter( Mandatory )]
+        [ValidateNotNullOrEmpty()]
+        [Microsoft.Data.SqlClient.SqlConnection] $InstanceConnection,
+
+        [Parameter()]
+        [ValidateNotNullOrEmpty()]
+        [string] $Name = ( [string](New-Guid) ).Substring(0, 8)
+    )
+
+    $Database = $Instance.PsObject.Copy()
+
+    Invoke-TSqlCommand "CREATE DATABASE [$Name]" -Connection $InstanceConnection
+
+    $Database | Add-Member InitialCatalog $Name
+    $Database.ConnectionString += ";Initial Catalog=$( $Database.InitialCatalog )"
+
+    Write-Output $Database
+}
